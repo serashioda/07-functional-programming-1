e8b83e8b0af09146fcd481b60ae108ec4da05ce7
Rick Patci <rpatci@gmail.com>
2016-08-23 09:16:47 -0700

    class-07

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -17,13 +17,13 @@ Article.prototype.toHtml = function(scriptTemplateId) {
 };
 
 Article.loadAll = function(dataWePassIn) {
-  /* NOTE: the original forEach code should be refactored
+  /* TODO: the original forEach code should be refactored
      using `.map()` -  since what we are trying to accomplish is the
      transformation of one collection into another. */
-  Article.allArticles = dataWePassIn.sort(function(a,b) {
+  inputData.sort(function(a,b) {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  }).map(function(ele) {
-    return new Article(ele);
+  }).forEach(function(ele) {
+    Article.allArticles.push(new Article(ele));
   });
 };
 
@@ -40,7 +40,6 @@ Article.fetchAll = function(nextFunction) {
       success: function(data, message, xhr) {
         var eTag = xhr.getResponseHeader('eTag');
         if (!localStorage.eTag || eTag !== localStorage.eTag) {
-          localStorage.eTag = eTag;
           Article.getAll(nextFunction); // DONE: pass 'nextFunction' into Article.getAll();
         } else {
           Article.loadAll(JSON.parse(localStorage.hackerIpsum));
@@ -55,49 +54,10 @@ Article.fetchAll = function(nextFunction) {
 };
 
 Article.getAll = function(nextFunction) {
-  $.getJSON('/data/hackerIpsum.json', function(responseData) {
+  $.getJSON('/data/hackerIpsum.json', function(responseData, message, xhr) {
+    localStorage.eTag = xhr.getResponseHeader('eTag');
     Article.loadAll(responseData);
     localStorage.hackerIpsum = JSON.stringify(responseData);
-    // DONE: invoke nextFunction!
-    nextFunction();
-  });
-};
-
-/* TODO: Chain together a `map` and a `reduce` call to get a rough count of
-    all words in all articles. */
-Article.numWordsAll = function() {
-  return Article.allArticles.map(function(article) {
-      //DONE: Grab the word count from each article body.
-    return article.body.match(/\w+/g).length;
-  })
-  // TODO: complete this reduce to get a grand total word count
-  .reduce(function() {
-  });
-};
-
-/* TODO: Chain together a `map` and a `reduce` call to
-          produce an array of *unique* author names. */
-Article.allAuthors = function() {
-  //return       TODO: map our collection
-    //return    TODO: return just the author names
-
-  /* TODO: For our `reduce` that we'll chain here -- since we are trying to
-      return an array, we'll need to specify an accumulator type...
-      What data type should this accumulator be and where is it placed? */
-};
-
-Article.numWordsByAuthor = function() {
-  /* TODO: Transform each author element into an object with 2 properties:
-      One for the author's name, and one for the total number of words across
-      the matching articles written by the specified author. */
-  return Article.allAuthors().map(function(author) {
-    return {
-      // name:
-      // numWords: someCollection.filter(function(curArticle) {
-      //  what do we return here to check for matching authors?
-      // })
-      // .map(...) // use .map to return the author's word count for each article's body (hint: regexp!).
-      // .reduce(...) // squash this array of numbers into one big number!
-    };
+    // TODO: invoke our parameter.
   });
 };
